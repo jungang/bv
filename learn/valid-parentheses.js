bd795beaa5dc3de5b8d3ebde1e791b1f105d93d8
jungang <jungang@vip.sina.com>
2018-08-08 18:05:19 +0800

    bump package version

--- learn/valid-parentheses.js
+++ learn/valid-parentheses.js
@@ -10,29 +10,47 @@
  */
 let isValid = function(s) {
 
+	console.log('s:', s);
+
 	let arr         = s.split('');
 	let strLeft     = '{[(';
-	let strRight    = ')]}';
+	let strRight    = '}])';
 	let stackLeft   = [];
-	let stackRight  = [];
-
+	let err         = false;
 	arr.forEach(function (item, index) {
 
 		if(strLeft.indexOf(item) !== -1){           //如果是左侧符号
-			stackLeft.push(item)
+			stackLeft.push(item)                    //将该符号压入左侧栈
 		}else {
 
-			console.log(stackLeft);
-			
-		}
+			if(stackLeft.length ===0){
+				err = true;
+			}
 
-	})
+			let leftIndex = strRight.indexOf(item);     //换算对应左侧符号索引位置
+
+			// strLeft[leftIndex]   取出符号
+			// stackLeft[stackLeft.length-1]   堆栈中的顶端符号
+			if(stackLeft[stackLeft.length-1] === strLeft[leftIndex]){   //判断是否对应
+				stackLeft.pop()
+			}
+		}
+	});
+	console.log('err:', err);
 	
+	return err || !stackLeft.length
 };
 
-isValid("()");
-isValid("()[]{}");
-isValid("([)]");
-isValid("{[]}");
+console.log(isValid("]"));
+// console.log(isValid("()[]{}"));
+// console.log(isValid("()"));
+// console.log(isValid("([)]"));
+// console.log(isValid("{[]}"));
+
+
+// isValid("()");
+// isValid("()[]{}");
+// isValid("([)]");
+// isValid("{[]}");
 
 
