242b6d13ea9b942fb07a8b128489ae4526a04ce1
jungang <jungang@vip.sina.com>
2018-11-06 19:30:08 +0800

    bump package version

--- learn/m2.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by jg on 2018/11/6.
- */
-
-
-import {foo} from './m1.js';
-console.log(foo);
-setTimeout(() => console.log(foo), 500);
---
+++ learn/vimlearn.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * Created by jg on 2018/11/6.
+ */
+
+
+let c = 'alldhhhckfefgkjlajslfjldsf..............';
+
+ikihkksdfasdf
+
+
+let a = {
+
+ss`
