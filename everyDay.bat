d7f2aebfed515ef7c2f884ab670825e66750ae87
jungang <bjsongjungang@corp.netease.com>
2018-12-20 15:00:43 +0800

    bump package version

--- everyDay.bat
+++ everyDay.bat
@@ -1,9 +1,4 @@
 @echo off
-if "%1" == "h" goto begin
-mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
-:begin
-
-
 cd G:\ÍøÒ×\ÏîÄ¿\gulp-git-push && G:
 npm run bump_version_patch_0.0.1
  
